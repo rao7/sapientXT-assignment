d090709b74bdd259426760aa00399ba25c60adac
Rahul Rao <rahulraoit@gmail.com>
2018-12-12 16:21:21 +0530

    money dispencer UI
    UI of money dispencer

---
+++ atm-money-dispencer/js/app.js
@@ -0,0 +1,8 @@
+var getAmount = {
+    notes : [1,2,5,10,20,50,100,200,500,2000],
+    dispenceNotes : function(amt){
+        console.log(amt);
+       var getValue = document.getElementById(amt).value;
+       console.log(getValue)
+    }
+}
