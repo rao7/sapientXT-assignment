1d0f405db80605e2cfb8aa6b74eb8c0111664351
Rahul Rao <rahulraoit@gmail.com>
2018-12-12 22:04:48 +0530

    logic for money dispenser

--- atm-money-dispencer/js/app.js
+++ atm-money-dispencer/js/app.js
@@ -1,8 +1,19 @@
 var getAmount = {
     notes : [1,2,5,10,20,50,100,200,500,2000],
     dispenceNotes : function(amt){
-        console.log(amt);
-       var getValue = document.getElementById(amt).value;
-       console.log(getValue)
+      var getAmt = document.getElementById(amt).value;
+	
+	for(i=notes.length-1; i>=0 ; i--){ 
+	var remainder = 0; 
+	if(getAmt >= notes[i]){
+		var remainder = getAmt % notes[i];
+		var quotient = Math.floor(getAmt / notes[i]); 
+		getAmt = remainder;
+		//console.log(remainder);
+		console.log(quotient+' notes of '+ notes[i]+' rs');
+		
+	}else{
+	console.log('0 notes of '+ notes[i]+' rs');
+	}	
     }
 }
