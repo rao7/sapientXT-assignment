b56e4d81cca2906dcd977dde1f9bc60b1454cda7
Rahul Rao <rahulraoit@gmail.com>
2018-12-13 14:33:52 +0530

    final js push

--- atm-money-dispencer/js/app.js
+++ atm-money-dispencer/js/app.js
@@ -1,19 +1,38 @@
 var getAmount = {
     notes : [1,2,5,10,20,50,100,200,500,2000],
-    dispenceNotes : function(amt){
-      var getAmt = document.getElementById(amt).value;
-	
-	for(i=notes.length-1; i>=0 ; i--){ 
-	var remainder = 0; 
-	if(getAmt >= notes[i]){
-		var remainder = getAmt % notes[i];
-		var quotient = Math.floor(getAmt / notes[i]); 
-		getAmt = remainder;
-		//console.log(remainder);
-		console.log(quotient+' notes of '+ notes[i]+' rs');
-		
-	}else{
-	console.log('0 notes of '+ notes[i]+' rs');
-	}	
+    dispenceNotes : function(amt , noteDisplay , notesTotal){
+        var getAmt = amt , total_noteCount = 0;
+        
+        if(typeof amt != 'number' && !amt) { alert('Empty or Invalid input'); return false;}
+        
+            noteDisplay.innerHTML = '';
+        
+        for(i= this.notes.length-1; i>=0 ; i--){ 
+            var remainder = 0; 
+            
+            if(getAmt >= this.notes[i]){
+                var remainder = getAmt % this.notes[i];
+                var quotient = Math.floor(getAmt / this.notes[i]); 
+                getAmt = remainder;
+                total_noteCount += quotient ;
+                this.template(quotient,this.notes[i],noteDisplay);
+                
+            }else{  this.template(0,this.notes[i],noteDisplay); }	
+
+            }
+
+            notesTotal.innerHTML = total_noteCount;
+    
+    },
+
+    template : function(noteCount , currentNote , targetEle ){ // templating function
+        var listItem = document.createElement('li');
+        var attr =  document.createAttribute("class");
+        attr.value = 'row__half fs-14'
+        listItem.setAttributeNode(attr);
+        var textNode = document.createTextNode(noteCount+ ' note of Rs ' +currentNote);
+        listItem.appendChild(textNode);
+    
+        targetEle.insertBefore(listItem, targetEle.childNodes[0]);
     }
 }
