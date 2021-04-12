a76707e03eadf63af31309923609b5a10a8816db
Rahul Rao <rahul.rao@publicissapient.com>
2021-04-13 01:26:25 +0530

    added build

---
+++ build/precache-manifest.5319505ae07eb8525b9855f1adb499ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdde4bc55726f61954069c6eae5cdd1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe3b98a9fae1e44d0344",
+    "url": "/static/css/main.f74abc3a.chunk.css"
+  },
+  {
+    "revision": "fda0120946737351dc3f",
+    "url": "/static/js/2.800282b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.800282b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3b98a9fae1e44d0344",
+    "url": "/static/js/main.5395729b.chunk.js"
+  },
+  {
+    "revision": "30328cf2e3b7858a2c10",
+    "url": "/static/js/runtime-main.64978e45.js"
+  },
+  {
+    "revision": "78bc7fb3021e4287481f7897eea345b7",
+    "url": "/static/media/logo.78bc7fb3.svg"
+  }
+]);
